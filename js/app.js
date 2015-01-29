173744b02aac71ad1f2ffad6dd06dca52148e5d3
stephenwoods <stephen.woods@nativex.com>
2015-01-29 11:00:58 -0600

    Adding files and getting hello world up.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function() {
+	var app = angular.module('LFViewer');
+    /*
+     * It's possible I'm doing this wrong, but I'll need to access a list of properties throughout all of these tabs, so I'm putting initialization logic and storing stuff in the root scope.
+     */
+	app.run(function($rootScope) {
+	});
+})();
